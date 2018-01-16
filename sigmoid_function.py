897fb843ab71fe6e545d9e7e9d9d250f1ba42e13
Shaun <seyyes2000@hotmail.com>
2018-01-16 15:07:16 -0500

    add more numpy code

--- sigmoid_function.py
+++ sigmoid_function.py
@@ -1,8 +1,24 @@
 # Sigmoid function also called a logistic function
 
 import math
+import numpy as np
 
 
+# Use math.exp() for calc sigmoid for one input value
 x = 3
 sig = 1 / ( 1 + math.exp(-x) )
-print ("Sigmoid value =", sig)
+print ("\nSigmoid value =", sig)
+
+# Use Numpy (array and exp) to cal sigmoid with multiple inputs from array
+x = np.array([1, 2, 3])
+sig = 1 / ( 1 + np.exp(-x) )
+print ("\nSigmoid numpy array =", sig)
+
+# Example of vector operation
+print ("\nVector + 3 =", (x + 3))
+
+
+# Calculating Sigmoid Gradient
+
+
+
