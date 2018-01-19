ad1ca6428fed14b376c4ac8519315c3c8f14876a
Shaun <seyyes2000@hotmail.com>
2018-01-19 13:22:14 -0500

    broadcasting example with softmax func

---
+++ broadcasting_softmax_func.py
@@ -0,0 +1,43 @@
+# A very important concept to understand in numpy is "broadcasting". 
+# It is very useful for performing mathematical operations 
+# between arrays of different shapes.
+
+
+import numpy as np
+
+
+def softmax(x):
+    """Calculates the softmax for each row of the input x.
+
+    Your code should work for a row vector and also for matrices of shape (n, m).
+
+    Argument:
+    x -- A numpy matrix of shape (n, m)
+
+    Returns:
+    s -- A numpy matrix equal to the softmax of x, of shape (n, m)
+    """
+    
+    ### START CODE HERE ### (≈ 3 lines of code)
+    # Apply exp() element-wise to x. Use np.exp(...).  (Sigmoid Function)
+    x_exp = np.exp(x)
+
+    # Create a vector x_sum that sums each row of x_exp. Use np.sum(..., axis = 1, keepdims = True).
+    x_sum = np.sum(x_exp, axis = 1, keepdims = True)
+    
+    # Compute softmax(x) by dividing x_exp by x_sum. It should automatically use numpy broadcasting.
+    s = x_exp / x_sum
+
+    ### END CODE HERE ###
+    
+    return s
+
+
+x = np.array([
+    [9, 2, 5, 0, 0],
+    [7, 5, 0, 0 ,0]])
+
+print("\nsoftmax(x) = " + str(softmax(x)))
+
+
+
