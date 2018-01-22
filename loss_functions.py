7f19f98af2c51d5b1cc6d4b5317d2d3aa854e58f
Shaun <seyyes2000@hotmail.com>
2018-01-22 14:37:05 -0500

    finish loss functions L1 and L2

--- loss_functions.py
+++ loss_functions.py
@@ -18,12 +18,43 @@ def L1(yhat, y):
     """
     
     ### START CODE HERE ### (≈ 1 line of code)
-    loss = abs(yhat - y)
+    loss = np.sum( abs(y - yhat) )
     ### END CODE HERE ###
     
     return loss
 
 
-yhat = np.array([.9, 0.2, 0.1, .4, .9])
+yhat = np.array([0.9, 0.2, 0.1, 0.4, 0.9])
 y = np.array([1, 0, 0, 1, 1])
-print("L1 = " + str(L1(yhat,y)))
+print("\nL1 = " + str(L1(yhat,y)))
+
+
+# There are several way of implementing the L2 loss, 
+# but you may find the function np.dot() useful. 
+# As a reminder, if x=[x1,x2,...,xn], then np.dot(x,x) .... 
+def L2(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+    
+    Returns:
+    loss -- the value of the L2 loss function defined above
+    """
+    
+    ### START CODE HERE ### (≈ 1 line of code)
+    z = y - yhat
+    loss = np.dot(z, z)
+    ### END CODE HERE ###
+    
+    return loss
+
+
+yhat = np.array([0.9, 0.2, 0.1, 0.4, 0.9])
+y = np.array([1, 0, 0, 1, 1])
+print("\nL2 = " + str(L2(yhat,y)))
+
+
+
+
+
