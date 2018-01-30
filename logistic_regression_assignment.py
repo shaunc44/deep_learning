9b888d69a49d26af1a6d49c52a9bb3b0e7737804
Shaun <seyyes2000@hotmail.com>
2018-01-30 09:29:58 -0500

    updates to assignment 1

--- logistic_regression_assignment.py
+++ logistic_regression_assignment.py
@@ -143,7 +143,7 @@ sigmoid(wTx+b) = 1 / 1 + e−(wTx+b) to make predictions. Use np.exp().
 """
 
 
-# GRADED FUNCTION: sigmoid
+# GRADED FUNCTION: sigmoid (logistic activation function)
 
 def sigmoid(z):
     """
@@ -161,6 +161,8 @@ def sigmoid(z):
     ### END CODE HERE ###
     
     return s
+
+print ("sigmoid([0, 2]) = " + str(sigmoid(np.array([0,2]))))
 
 
 """
@@ -190,13 +192,87 @@ def initialize_with_zeros(dim):
 
     assert(w.shape == (dim, 1))
     assert(isinstance(b, float) or isinstance(b, int))
-    
     return w, b
 
 
+dim = 2
+w, b = initialize_with_zeros(dim)
+print ("w = " + str(w))
+print ("b = " + str(b))
 
 
+"""
+4.3 - Forward and Backward propagation
 
+Now that your parameters are initialized, you can do the "forward" and 
+"backward" propagation steps for learning the parameters.
+
+Exercise: Implement a function propagate() that computes the cost function and its gradient.
+
+Hints:
+
+Forward Propagation:
+
+    You get X
+    You compute A = σ(wTX+b) = ( a(0),a(1),...,a(m−1),a(m) )
+    You calculate the cost function
+"""
+
+# GRADED FUNCTION: propagate
+
+def propagate(w, b, X, Y):
+    """
+    Implement the cost function and its gradient for the propagation explained above
+
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat) of size (1, number of examples)
+
+    Return:
+    cost -- negative log-likelihood cost for logistic regression
+    dw -- gradient of the loss with respect to w, thus same shape as w
+    db -- gradient of the loss with respect to b, thus same shape as b
+    
+    Tips:
+    - Write your code step by step for the propagation. np.log(), np.dot()
+    """
+    
+    m = X.shape[1]
+    
+    # FORWARD PROPAGATION (FROM X TO COST)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    A = None                                    # compute activation
+    cost = None                                 # compute cost
+    ### END CODE HERE ###
+    
+    # BACKWARD PROPAGATION (TO FIND GRAD)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    dw = None
+    db = None
+    ### END CODE HERE ###
+
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+    
+    grads = {"dw": dw,
+             "db": db}
+
+    return grads, cost
+
+
+w, b, X, Y = np.array([[1.],
+                       [2.]]), 
+                       2., 
+                       np.array([[1.,2.,-1.],[3.,4.,-3.2]]), 
+                       np.array([[1,0,1]])
+grads, cost = propagate(w, b, X, Y)
+print ("dw = " + str(grads["dw"]))
+print ("db = " + str(grads["db"]))
+print ("cost = " + str(cost))
 
 
 
