99c595c81e7b42b336808c62d5a7f667cce4464a
Shaun <seyyes2000@hotmail.com>
2018-02-02 16:36:50 -0500

    start optimize func

--- logistic_regression_assignment.py
+++ logistic_regression_assignment.py
@@ -285,6 +285,124 @@ print ("db = " + str(grads["db"]))
 print ("cost = " + str(cost))
 
 
+"""
+d) Optimization
+
+    You have initialized your parameters.
+    You are also able to compute a cost function and its gradient.
+    Now, you want to update the parameters using gradient descent.
+
+Exercise: Write down the optimization function. 
+The goal is to learn w and b by minimizing the cost function J. 
+For a parameter θ (theta), the update rule is θ = θ − α*dθ, where α is the learning rate.
+"""
+
+
+# GRADED FUNCTION: optimize
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    """
+    This function optimizes w and b by running a gradient descent algorithm
+    
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of shape (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat), of shape (1, number of examples)
+    num_iterations -- number of iterations of the optimization loop
+    learning_rate -- learning rate of the gradient descent update rule
+    print_cost -- True to print the loss every 100 steps
+
+    Returns:
+    params -- dictionary containing the weights w and bias b
+    grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
+    costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
+
+    Tips:
+    You basically need to write down two steps and iterate through them:
+        1) Calculate the cost and the gradient for the current parameters. Use propagate().
+        2) Update the parameters using gradient descent rule for w and b.
+    """
+
+    costs = []
+
+    for i in range(num_iterations):
+
+        # Cost and gradient calculation (≈ 1-4 lines of code)
+        ### START CODE HERE ### 
+        grads, cost = propagate(w, b, X, Y)
+        ### END CODE HERE ###
+        
+        # Retrieve derivatives from grads
+        dw = grads["dw"]
+        db = grads["db"]
+        
+        # update rule (≈ 2 lines of code)
+        ### START CODE HERE ###
+        w = w - np.dot(learning_rate, dw)
+        b = b - np.dot(learning_rate, db)
+        ### END CODE HERE ###
+        
+        # Record the costs
+        if i % 100 == 0:
+            costs.append(cost)
+        
+        # Print the cost every 100 training examples
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+    
+    params = {"w": w,
+              "b": b}
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return params, grads, costs
+
+
+params, grads, costs = optimize(w, b, X, Y, 
+                                num_iterations= 100, 
+                                learning_rate = 0.009, 
+                                print_cost = False)
+
+print ("w = " + str(params["w"]))
+print ("b = " + str(params["b"]))
+print ("dw = " + str(grads["dw"]))
+print ("db = " + str(grads["db"]))
+
+"""
+Expected Output:
+w   [[ 0.19033591] [ 0.12259159]]
+b   1.92535983008
+dw  [[ 0.67752042] [ 1.41625495]]
+db  0.219194504541
+"""
+
+
+"""
+Exercise: 
+The previous function will output the learned w and b. 
+We are able to use w and b to predict the labels for a dataset X. 
+Implement the predict() function. 
+
+There are two steps to computing predictions:
+
+    1. Calculate Ŷ = A = σ(wTX + b)
+
+    2. Convert the entries of a into 0 (if activation <= 0.5) 
+    or 1 (if activation > 0.5), stores the predictions in a vector Y_prediction. 
+    If you wish, you can use an if/else statement in a for-loop 
+    (though there is also a way to vectorize this).
+"""
+
+
+
+
+
+
+
+
+
 
 
 
