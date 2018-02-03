b94d0a6d4966ac09fca27ddc029dde15f69185f1
Shaun <seyyes2000@hotmail.com>
2018-02-03 13:26:57 -0500

    begin work on predict function

--- logistic_regression_assignment.py
+++ logistic_regression_assignment.py
@@ -396,6 +396,57 @@ There are two steps to computing predictions:
 """
 
 
+# GRADED FUNCTION: predict
+
+def predict(w, b, X):
+    '''
+    Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
+
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    
+    Returns:
+    Y_prediction -- a numpy array (vector) containing all predictions (0/1) for the examples in X
+    '''
+
+    m = X.shape[1]
+    Y_prediction = np.zeros((1,m))
+    print ("Y_prediction", Y_prediction)
+    w = w.reshape(X.shape[0], 1)
+
+    # Compute vector "A" predicting the probabilities of a cat being present in the picture
+    ### START CODE HERE ### (≈ 1 line of code)
+    A = sigmoid(X)
+    #A = np.add(A)
+    #A = np.add(A[0][0] + A[0][1])
+    print ("A =", A)
+    print ("A.shape[1] =", A.shape[1])
+    a1 = A[0][0]
+    a2 = A[0][1]
+    print (np.add(a1, a2))
+    print (np.where(A > 0.5, 1., 0.))
+    ### END CODE HERE ###
+
+    for i in range(A.shape[1]):
+        
+        # Convert probabilities A[0,i] to actual predictions p[0,i]
+        ### START CODE HERE ### (≈ 4 lines of code)
+        A
+    Y_prediction = np.where(A > 0.5, 1. ,0.)
+        ### END CODE HERE ###
+    
+    print ("Y_prediction =", Y_prediction)
+    
+    assert(Y_prediction.shape == (1, m))
+    
+    return Y_prediction
+
+w = np.array([[0.1124579],[0.23106775]])
+b = -0.3
+X = np.array([[1.,-1.1,-3.2],[1.2,2.,0.1]])
+print ("predictions = " + str(predict(w, b, X)))
 
 
 
